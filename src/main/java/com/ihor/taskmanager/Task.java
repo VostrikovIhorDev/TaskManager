45dff352e0a8f6f18f702faadc56f6d456ebcdb5
Ihor Vostrikov <vostrikov.ihor.dev@gmail.com>
2026-09-06 22:46:37 +0200

    Implement task domain model

--- src/main/java/com/ihor/taskmanager/Task.java
+++ src/main/java/com/ihor/taskmanager/Task.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 
 public class Task {
 
-    private Integer id;
+    private Long id;
     private String title;
     private String description;
     private TaskStatus status;
@@ -13,11 +13,57 @@ public class Task {
 
 
     public Task(String title, String description,  TaskPriority priority ) {
+        if (title == null || title.isBlank()) {
+            throw  new IllegalArgumentException("wrong title");
+        }
+        if (priority == null) {
+            throw  new IllegalArgumentException("wrong priority");
+        }
         this.title = title;
         this.description = description;
         this.status = TaskStatus.TODO;
         this.priority = priority;
         this.createdAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public TaskPriority getPriority() {
+        return priority;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public void setPriority(TaskPriority priority) {
+        this.priority = priority;
     }
 }
 
---
+++ src/test/java/com/ihor/taskmanager/TaskTest.java
@@ -0,0 +1,4 @@
+package com.ihor.taskmanager;
+
+public class TaskTest {
+}
