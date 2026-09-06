7b2a77b778c80cce405577190885011119b24e84
Ihor Vostrikov <vostrikov.ihor.dev@gmail.com>
2026-09-06 23:33:07 +0200

    add Task unit test

--- src/test/java/com/ihor/taskmanager/TaskTest.java
+++ src/test/java/com/ihor/taskmanager/TaskTest.java
@@ -1,4 +1,30 @@
 package com.ihor.taskmanager;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TaskTest {
+
+    @Test
+    void shouldCreateTask() {
+      Task task = new Task("next Task", "do something", TaskPriority.LOW);
+      assertEquals("next Task" , task.getTitle());
+      assertEquals("do something", task.getDescription());
+      assertEquals(TaskPriority.LOW, task.getPriority());
+      assertEquals(TaskStatus.TODO, task.getStatus());
+      assertNotNull(task.getCreatedAt());
+      assertNull(task.getId());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenTitleIsInvalid() {
+        assertThrows(IllegalArgumentException.class, () -> new Task("", "do something", TaskPriority.LOW));
+        assertThrows(IllegalArgumentException.class, () -> new Task(null, "do something", TaskPriority.LOW));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenPriorityIsNull() {
+        assertThrows(IllegalArgumentException.class, ()-> new Task("next Task", "do something", null));
+    }
 }
